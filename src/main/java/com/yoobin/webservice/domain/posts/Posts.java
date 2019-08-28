96f6fb3d6027a72b0b2790a0f2f9e965b2a6407d
yoobin2486 <yoobin2486@naver.com>
2019-08-28 15:37:36 +0900

    JPA로 간단한 API만들기

---
+++ src/main/java/com/yoobin/webservice/domain/posts/Posts.java
@@ -0,0 +1,35 @@
+package com.yoobin.webservice.domain.posts;
+
+import com.yoobin.webservice.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Posts extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
