ffcb60ddd6578c02efb4a17735a4f97211fc9122
yoobin2486 <yoobin2486@naver.com>
2019-08-29 14:49:26 +0900

    Springboot&Handlebars로 화면 만들기(1)

---
+++ src/main/java/com/yoobin/webservice/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.yoobin.webservice.service;
+
+import com.yoobin.webservice.domain.posts.PostsRepository;
+import com.yoobin.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto) {
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+}
--- src/main/java/com/yoobin/webservice/web/WebRestController.java
+++ src/main/java/com/yoobin/webservice/web/WebRestController.java
@@ -2,6 +2,7 @@ package com.yoobin.webservice.web;
 
 import com.yoobin.webservice.domain.posts.PostsRepository;
 import com.yoobin.webservice.dto.posts.PostsSaveRequestDto;
+import com.yoobin.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
+    private PostsService postsService;
     private PostsRepository postsRepository;
 
     @GetMapping("/hello")
@@ -20,7 +22,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
     }
 }
