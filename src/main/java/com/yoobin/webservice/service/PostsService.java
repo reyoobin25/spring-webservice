169553670559a084512d49f6af9d070eb14d8230
yoobin2486 <yoobin2486@naver.com>
2019-09-03 11:29:04 +0900

    Handlebars(2) - 등록 및 화면 출력

--- src/main/java/com/yoobin/webservice/service/PostsService.java
+++ src/main/java/com/yoobin/webservice/service/PostsService.java
@@ -1,11 +1,14 @@
 package com.yoobin.webservice.service;
 
 import com.yoobin.webservice.domain.posts.PostsRepository;
+import com.yoobin.webservice.dto.posts.PostsMainResponseDto;
 import com.yoobin.webservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -15,5 +18,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<PostsMainResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
